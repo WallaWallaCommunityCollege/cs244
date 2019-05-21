c2039c911fbf7eec6598ae360ae28f5abf11fe21
Michael Cummings <dragonrun1@gmail.com>
2019-05-21 11:48:58 -0700

    Updated to to use Heroku port env var.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8000;
 app.use(express.static('./dist'));
-app.listen(8000, () => console.log('ready'));
+app.listen(port, () => console.log('ready'));
